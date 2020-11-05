d7e147a94d075461d6c15184c85216d081737f12
Peter Marchese <31332889+Pmarch365@users.noreply.github.com>
2020-11-04 22:20:53 -0500

    changed values for player and AI characters

--- MapCreator.py
+++ MapCreator.py
@@ -10,32 +10,32 @@ def mapGen(d):
         return
 
     #Initiate map with all 1 chars for regular cell
-    map = [[1 for i in range(d)] for j in range(d)]
+    map = [[0 for i in range(d)] for j in range(d)]
 
     #Add characters to top row
     tracker = 0
     for i in range(len(map[0])):
         if tracker == 0:
-            map[0][i] = 12
+            map[0][i] = 2
             tracker = 1
         elif tracker == 1:
-            map[0][i] = 13
+            map[0][i] = 1
             tracker = 2
         else:
-            map[0][i] = 14
+            map[0][i] = 3
             tracker = 0
 
     #Add characters to bottom row
     tracker = 0
     for i in range(len(map[d-1])):
         if tracker == 0:
-            map[d-1][i] = 2
+            map[d-1][i] = 5
             tracker = 1
         elif tracker == 1:
-            map[d-1][i] = 3
+            map[d-1][i] = 4
             tracker = 2
         else:
-            map[d-1][i] = 4
+            map[d-1][i] = 6
             tracker = 0
 
     #Add pits
@@ -46,8 +46,8 @@ def mapGen(d):
             continue
         j = 0
         while placed_pits != pits_per_row:
-            if random.randint(1,d) == 1 and map[i][j] != 0:
-                map[i][j] = 0
+            if random.randint(1,d) == 1 and map[i][j] != 7:
+                map[i][j] = 7
                 placed_pits += 1
             j += 1
             if j >= d:
@@ -55,4 +55,4 @@ def mapGen(d):
         placed_pits = 0
     return map
 
-#print(mapGen(9))
+print(mapGen(9))
