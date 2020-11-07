d395ba052c958b91c2a1657f0c19bf67a6d84d6f
Peter Marchese <31332889+Pmarch365@users.noreply.github.com>
2020-11-07 02:41:09 -0500

    My algorithm now sorts children by their heuristics

--- peterMiniMax.py
+++ peterMiniMax.py
@@ -50,21 +50,19 @@ def battle(node, child):
             return -2
 
 def sort_neighbors(map, node, temp):
-    neighbors = []
+    list = []
     for i in range(len(temp)):
         if temp[i][0] >= len(map) or temp[i][1] >= len(map[0]) or temp[i][0] < 0 or temp[i][1] < 0:
             continue
         child = Node(temp[i][0], temp[i][1], map[temp[i][0]][temp[i][1]])
-        neighbors.append( ( (temp[i][0], temp[i][1]), battle(node, child) ) )
+        list.append( ( (temp[i][0], temp[i][1]), battle(node, child) ) )
 
-    neighbors.sort(key = lambda x: x[1])
+    list.sort(key = lambda x: x[1])
 
-    for i in range(len(neighbors)):
-        neighbors[i] = neighbors[i][0]
+    for i in range(len(list)):
+        list[i] = list[i][0]
 
-    neighbors.reverse
-
-    return neighbors
+    return list
 
 def takeTurn(map):
     scores = []
@@ -72,7 +70,7 @@ def takeTurn(map):
         for j in range(len(map[i])):
             if map[i][j] == 4 or map[i][j] == 5 or map[i][j] == 6:
                 node = Node(i,j, map[i][j])
-                scores.append(MiniMax(map, node, node, 9, 1, -math.inf, math.inf))  # This begins the min max search with alpha beta pruning
+                scores.append(MiniMax(map, node, node, 20, 1, 0, 0))  # This begins the min max search with alpha beta pruning
     return scores
 
 def MiniMax(map, node, child, depth, playerNum, a, b):
@@ -81,7 +79,9 @@ def MiniMax(map, node, child, depth, playerNum, a, b):
         neighbors_temp = [(child.x-1, child.y), (child.x+1, child.y), (child.x, child.y-1), (child.x, child.y+1), 
         (child.x-1, child.y+1), (child.x-1, child.y-1), (child.x+1, child.y+1), (child.x+1, child.y-1)]
 
-        neighbors = sort_neighbors(map, node, neighbors_temp)
+        neighbors = sort_neighbors(map, node, neighbors_temp)[::-1]
+
+        print(neighbors)
 
         if depth == 0 or battle(node, child) == 2:
             return battle(node,child)
@@ -103,10 +103,6 @@ def MiniMax(map, node, child, depth, playerNum, a, b):
                 if b <= a:
                     break
             return val
-#map = MapCreator.mapGen(9)
-map = [
-    [5,4,6],
-    [0,3,0],
-    [2,1,0]
-]
+map = MapCreator.mapGen(3)
+print(map)
 print(takeTurn(map))
